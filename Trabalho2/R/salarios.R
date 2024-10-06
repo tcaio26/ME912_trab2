48bb85827176ef86cc9af54daac43c99b11c513e
tcaio26 <167098967+tcaio26@users.noreply.github.com>
2024-10-06 13:49:23 -0300

    Add files via upload

---
+++ Trabalho2/R/salarios.R
@@ -0,0 +1,18 @@
+#' Salários e dados pessoais simulados
+#'
+#' Dados simulados para servirem de exemplo para as funções do pacote "trabalho2"
+#' Todas as variáveis foram simuladas aleatóriamente, com a semente 247005. Mais detalhes podem ser encontrados no arquivo "data-raw/salarios.R"
+#'
+#' @format ## salarios
+#' Um data frame com 1000 linhas e 7 colunas:
+#' \describe{
+#'    \item {id} Número de identificação da pessoa
+#'    \item {idade} Idade da pessoa
+#'    \item {altura} Altura da pessoa
+#'    \item {peso} Peso da pessoa
+#'    \item {anos_estudando} Anos que a pessoa passou estudando (escola, ensino médio, faculdade, etc)
+#'    \item {filhos} Número de filhos
+#'    \item {salario} Salário
+#' }
+#' @source data-raw/salarios.R
+"salarios"
